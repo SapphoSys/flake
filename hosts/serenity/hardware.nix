64c956ca7f62a8141bc6ce55b476f5d20c00d9ff
Chloe A <chloe@sapphic.moe>
2026-02-08 02:51:52 +0500

    chore(hosts:serenity): changed hardware.nix

---
+++ hosts/serenity/hardware.nix
@@ -0,0 +1,58 @@
+{
+  config,
+  lib,
+  modulesPath,
+  ...
+}:
+
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+    timeout = 3;
+  };
+
+  # Kernel modules
+  boot.initrd.availableKernelModules = [
+    "nvme"
+    "xhci_pci"
+    "usb_storage"
+    "usbhid"
+    "sd_mod"
+    "sdhci_pci"
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/990d6165-6ad6-4ebd-b56b-3b4c5c1190b0";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/290F-060B";
+    fsType = "vfat";
+    options = [
+      "fmask=0022"
+      "dmask=0022"
+    ];
+  };
+
+  # Swap configuration
+  swapDevices = [ ];
+
+  # CPU configuration
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # Enable firmware updates
+  hardware.enableRedistributableFirmware = true;
+
+  # Networking
+  networking.useDHCP = lib.mkDefault true;
+}
