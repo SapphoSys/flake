02523dcb449b964d54ecbf63ac696614924e2de6
Chloe A <chloe@sapphic.moe>
2026-02-12 04:47:32 +0500

    chore(hosts:serenity): test trimmed kernel again(??)

--- modules/nixos/hardware/trimmed-jovian-kernel.nix
+++ modules/nixos/hardware/trimmed-jovian-kernel.nix
@@ -9,16 +9,8 @@
     nixpkgs.overlays = [
       (final: super: {
         linux_jovian = super.linux_jovian.overrideAttrs (old: {
-          # Update the config file reference
           configfile = ./trimmed-jovian-kernel.config;
-
-          preConfigure = ''
-            echo "Using trimmed Jovian kernel config"
-            cp ${./trimmed-jovian-kernel.config} .config
-          ''
-          + (old.preConfigure or "");
-
-          name = "linux-${old.version or "6.16.12"}-valve9-jovian1-trimmed";
+          name = "linux-${old.version}-jovian-trimmed";
         });
       })
     ];
