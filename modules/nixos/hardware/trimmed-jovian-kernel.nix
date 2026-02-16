f250ee200c7e1c340762a4d61af8f561463e2b6a
Chloe A <chloe@sapphic.moe>
2026-02-17 02:15:08 +0500

    refactor(modules:steamdeck): try approach 3.5 for kernel trimming

--- modules/nixos/hardware/trimmed-jovian-kernel.nix
+++ modules/nixos/hardware/trimmed-jovian-kernel.nix
@@ -21,32 +21,20 @@
         patch = null;
         structuredExtraConfig = with lib.kernel; {
           # ===== GPU Drivers =====
-          # Disable NVIDIA drivers (nouveau)
+          # Disable NVIDIA drivers (nouveau) - biggest time saver
           DRM_NOUVEAU = lib.mkForce no;
-          DRM_NOVA = lib.mkForce no;
 
-          # Disable Intel graphics drivers
+          # Disable Intel graphics drivers - also big time savers
           DRM_I915 = lib.mkForce no;
           DRM_XE = lib.mkForce no;
 
-          # Disable other GPU vendors (keep AMD only)
-          DRM_RADEON = lib.mkForce no; # Older AMD cards
-          DRM_GMA500 = lib.mkForce no; # Intel GMA
-          DRM_MGAG200 = lib.mkForce unset; # Matrox
-          DRM_AST = lib.mkForce unset; # ASPEED
-          DRM_BOCHS = lib.mkForce unset; # QEMU
-          DRM_CIRRUS_QEMU = lib.mkForce unset;
-          DRM_QXL = lib.mkForce unset; # QEMU/Xen
-          DRM_VIRTIO_GPU = lib.mkForce unset; # Virtualization
-          DRM_VMWGFX = lib.mkForce unset; # VMware
-
           # ===== WiFi Drivers =====
-          # Disable all WiFi vendors except what Steam Deck uses
+          # Disable Atheros WiFi (Steam Deck uses MediaTek)
+          WLAN_VENDOR_ATH = lib.mkForce no; # Big time saver!
+
+          # Disable other WiFi vendors
           WLAN_VENDOR_ADMTEK = lib.mkForce no;
-          WLAN_VENDOR_ATH = lib.mkForce no; # Atheros (big time saver!)
           WLAN_VENDOR_ATMEL = lib.mkForce no;
-          WLAN_VENDOR_BROADCOM = lib.mkForce no;
-          WLAN_VENDOR_CISCO = lib.mkForce no;
           WLAN_VENDOR_INTEL = lib.mkForce no;
           WLAN_VENDOR_INTERSIL = lib.mkForce no;
           WLAN_VENDOR_MARVELL = lib.mkForce no;
@@ -60,56 +48,28 @@
           WLAN_VENDOR_ZYDAS = lib.mkForce no;
           WLAN_VENDOR_QUANTENNA = lib.mkForce no;
 
-          # ===== Media Drivers =====
-          # Steam Deck has no TV tuners, webcams, or capture cards
-          MEDIA_ANALOG_TV_SUPPORT = lib.mkForce no;
-          MEDIA_DIGITAL_TV_SUPPORT = lib.mkForce no;
-          MEDIA_RADIO_SUPPORT = lib.mkForce no;
-          MEDIA_SDR_SUPPORT = lib.mkForce no;
-          MEDIA_CEC_SUPPORT = lib.mkForce no;
-          DVB_CORE = lib.mkForce unset;
-
           # ===== Other Unnecessary Hardware =====
           NFC = lib.mkForce no; # No NFC chip
           HAMRADIO = lib.mkForce no; # No amateur radio
           STAGING = lib.mkForce no; # Experimental/unmaintained drivers (huge!)
-          RC_CORE = lib.mkForce unset; # Infrared remote controls
-          LIRC = lib.mkForce unset; # IR transceivers
+          ISDN = lib.mkForce no; # ISDN modems (obsolete)
 
-          # Old/obscure ethernet cards
+          # Only disable ethernet vendors without complex dependencies
           NET_VENDOR_3COM = lib.mkForce no;
           NET_VENDOR_ADAPTEC = lib.mkForce no;
           NET_VENDOR_ALTEON = lib.mkForce no;
-          NET_VENDOR_BROADCOM = lib.mkForce no;
-          NET_VENDOR_BROCADE = lib.mkForce no;
-          NET_VENDOR_CHELSIO = lib.mkForce no;
-          NET_VENDOR_CISCO = lib.mkForce no;
           NET_VENDOR_DEC = lib.mkForce no;
           NET_VENDOR_DLINK = lib.mkForce no;
-          NET_VENDOR_EMULEX = lib.mkForce no;
           NET_VENDOR_GOOGLE = lib.mkForce no;
           NET_VENDOR_HUAWEI = lib.mkForce no;
-          NET_VENDOR_MARVELL = lib.mkForce no;
-          NET_VENDOR_MELLANOX = lib.mkForce no;
           NET_VENDOR_MYRI = lib.mkForce no;
           NET_VENDOR_NETERION = lib.mkForce no;
           NET_VENDOR_NVIDIA = lib.mkForce no;
           NET_VENDOR_OKI = lib.mkForce no;
-          NET_VENDOR_QLOGIC = lib.mkForce no;
           NET_VENDOR_SOLARFLARE = lib.mkForce no;
           NET_VENDOR_SUN = lib.mkForce no;
           NET_VENDOR_TEHUTI = lib.mkForce no;
-          NET_VENDOR_TI = lib.mkForce no;
           NET_VENDOR_VIA = lib.mkForce no;
-
-          # USB Serial adapters (keep core USB support)
-          USB_SERIAL_GENERIC = lib.mkForce unset;
-          USB_SERIAL_SIMPLE = lib.mkForce unset;
-
-          # Industrial/embedded systems
-          INFINIBAND = lib.mkForce unset; # InfiniBand networking (servers)
-          ISDN = lib.mkForce no; # ISDN modems (obsolete)
-          CAN = lib.mkForce unset; # CAN bus (automotive/industrial)
         };
       }
     ];
