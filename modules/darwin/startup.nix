2a93eb57a5c1a5c41903deb30f2cc67a6d787d13
Chloe <chloe@sapphic.moe>
2026-06-05 14:58:02 +0500

    chore(modules): add raycast to startup

--- modules/darwin/startup.nix
+++ modules/darwin/startup.nix
@@ -5,6 +5,7 @@
       "/Applications/1Password.app"
       "/Applications/Mos.app"
       "/Applications/Maccy.app"
+      "/Applications/Raycast.app"
       "/Users/chloe/Applications/Home Manager Apps/Shottr.app"
       "/Applications/Tailscale.app"
     ];
