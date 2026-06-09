fa29e0cb179cc33d23a7379efbcfd409dff881c8
Chloe <chloe@sapphic.moe>
2026-06-09 18:01:14 +0500

    chore(modules): remove maccy from darwin startup

--- modules/darwin/startup.nix
+++ modules/darwin/startup.nix
@@ -4,7 +4,6 @@
     items = [
       "/Applications/1Password.app"
       "/Applications/Mos.app"
-      "/Applications/Maccy.app"
       "/Applications/Raycast.app"
       "/Users/chloe/Applications/Home Manager Apps/Shottr.app"
       "/Applications/Tailscale.app"
